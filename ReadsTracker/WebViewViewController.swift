db7e0d19d23fe2a6971299da23b7ac3d1f6caa85
Danny McAteer <mcateerd2@gmail.com>
2017-11-18 09:45:57 -0500

    feat: Create webView viewController and storyboard file
    Todo: Implement web view navigation for book in bookDetailView

---
+++ ReadsTracker/WebViewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewViewController.swift
+//  BookTracker
+//
+//  Created by Daniel McAteer on 11/18/17.
+//  Copyright © 2017 Daniel McAteer. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+}
