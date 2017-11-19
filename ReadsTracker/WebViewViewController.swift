25ef6f456b7b2d45bcc39e66a7cf6a581416f307
Danny McAteer <mcateerd2@gmail.com>
2017-11-19 12:11:59 -0500

    feat: Add textField to navigationBar
    Add textField to navigationBar that allows user to submit new URL in shopping view

--- ReadsTracker/WebViewViewController.swift
+++ ReadsTracker/WebViewViewController.swift
@@ -12,21 +12,31 @@ import WebKit
 class WebViewViewController: UIViewController, WKNavigationDelegate {
     
     // MARK: - Constants and Variables
-    var webView: WKWebView!
+    var webView: WKWebView
+    @IBOutlet weak var barView: UIView!
+    
+    // MARK: - Intializers
+    required init?(coder aDecoder: NSCoder) {
+        self.webView = WKWebView(frame: .zero)
+        super.init(coder: aDecoder)
+    }
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Initialize webView
-        webView = WKWebView(frame: self.view.bounds)
-        webView.navigationDelegate = self
+        barView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
         
         // Add webView to main view
-        self.view.addSubview(webView)
+        view.addSubview(webView)
+        
+        // Add constraints to the webView
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let height = NSLayoutConstraint(item: webView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1, constant: 0)
+        let width = NSLayoutConstraint(item: webView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
+        view.addConstraints([height, width])
         
         // Load desired URL
-        let url = URL(string: "http://www.google.com")
+        let url = URL(string: "http://www.appcoda.com")
         let request = URLRequest(url: url!)
         webView.load(request)
     }
