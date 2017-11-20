6129b1d01f5aa0c406b4e4655e8a78bf67322243
Danny McAteer <mcateerd2@gmail.com>
2017-11-19 20:33:19 -0500

    fix: Constrain webView to bottom of barView
    The top of the webView was being obscured by the barView before setting the proper constraints

--- ReadsTracker/WebViewViewController.swift
+++ ReadsTracker/WebViewViewController.swift
@@ -14,6 +14,7 @@ class WebViewViewController: UIViewController, WKNavigationDelegate {
     // MARK: - Constants and Variables
     var webView: WKWebView
     @IBOutlet weak var barView: UIView!
+    @IBOutlet weak var urlTextField: UITextField!
     
     // MARK: - Intializers
     required init?(coder aDecoder: NSCoder) {
@@ -24,21 +25,31 @@ class WebViewViewController: UIViewController, WKNavigationDelegate {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        barView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
+        
+        // Add barView to view
+        view.addSubview(barView)
         
         // Add webView to main view
         view.addSubview(webView)
         
         // Add constraints to the webView
         webView.translatesAutoresizingMaskIntoConstraints = false
-        let height = NSLayoutConstraint(item: webView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1, constant: 0)
-        let width = NSLayoutConstraint(item: webView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
-        view.addConstraints([height, width])
+        let webViewTop = NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: barView, attribute: .bottom, multiplier: 1, constant: 0)
+        let webViewWidth = NSLayoutConstraint(item: webView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1, constant: 0)
+        let webViewBottm = NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        view.addConstraints([webViewTop, webViewWidth, webViewBottm])
         
         // Load desired URL
         let url = URL(string: "http://www.appcoda.com")
         let request = URLRequest(url: url!)
         webView.load(request)
+    }
+    
+    // MARK: - Delegate Methods
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        urlTextField.resignFirstResponder()
+        webView.load(URLRequest(url: URL(string: urlTextField.text!)!))
+        return false
     }
     
 }
